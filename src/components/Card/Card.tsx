90553297de872c61506c37f576d49d9f58b6ee73
Semion Krapivin <homer1996@gmail.com>
2022-10-14 02:45:43 +0300

    feat: add card data and assets

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import styles from './Card.module.css';
+import { cardArray } from './cardData';
 
 function Card() {
   return <div>Card</div>;
---
+++ src/components/Card/cardData.ts
@@ -0,0 +1,32 @@
+export const cardArray = [
+  {
+    title: 'Knight Armiger',
+    price: 60,
+    fracton: 'Chaos Knights',
+    imageUrl: '/assets/img/ck.jpg',
+  },
+  {
+    title: 'Battle Sisters Squad',
+    price: 50,
+    fracton: 'Adepta Sororitas',
+    imageUrl: '/assets/img/as.jpg',
+  },
+  {
+    title: 'Makari',
+    price: 15,
+    fracton: 'Orks',
+    imageUrl: '/assets/img/ork.jpg',
+  },
+  {
+    title: 'Trojan Valoris',
+    price: 40,
+    fracton: 'Adepta Custodes',
+    imageUrl: '/assets/img/ac.jpg',
+  },
+  {
+    title: 'Deathwing sergeant',
+    price: 30,
+    fracton: 'Dark Angels',
+    imageUrl: '/assets/img/da.jpg',
+  },
+];
