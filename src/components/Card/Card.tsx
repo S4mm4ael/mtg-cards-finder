f8bdc12ffb855e707b0b515f1d6ac834190624c5
Semion Krapivin <homer1996@gmail.com>
2022-10-15 11:18:40 +0300

    feat: all card render done

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import styles from './Card.module.css';
 import { cardArray } from './cardData';
 
-export function Card(): JSX.Element {
-  const id = 0;
+export function Card({ id }: { id: number }): JSX.Element {
   return (
     <div id={`card-${id + 1}`} className={styles.card}>
       <h5 className={styles.card__title}>{cardArray[id].title}</h5>
@@ -15,8 +14,8 @@ export function Card(): JSX.Element {
         alt={cardArray[id].title}
       />
 
-      <span className={styles.card__fraction}>Fraction:</span>
-      <b>{cardArray[id].fraction}</b>
+      <span className={styles.card__faction}>Faction:</span>
+      <b>{cardArray[id].faction}</b>
       <div>
         <div>
           <span className={styles.card__price}>Price:</span>
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Search from '../Search/Search';
+import styles from '../Card/Card.module.css';
 import { Card } from 'components/Card/Card';
+import { cardArray } from 'components/Card/cardData';
 
 function Main(): JSX.Element {
   return (
     <main>
       <Search />
-      <section className="card__section">
-        <Card />
+      <section className={styles.card__section}>
+        {cardArray.map((item, curId) => (
+          <Card key={item.title} id={curId} />
+        ))}
       </section>
     </main>
   );
