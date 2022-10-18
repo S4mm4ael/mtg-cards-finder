87112697c6a945d6c068c6f3dd6ec878b1b7574a
S4mm4el <homer1996@gmail.com>
2022-10-18 15:27:56 +0300

    feat: add About css

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import styles from './About.module.css';
 
 function About(): JSX.Element {
   return (
-    <section id="about-section" className="about">
+    <section id="about-section" className={styles.about__section}>
       <h3>Hi! My name is Semion</h3>
+      <p> I am new to React, this is my first study project</p>
     </section>
   );
 }
