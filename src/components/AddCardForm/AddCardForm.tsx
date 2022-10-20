91b391129750dd33b7af0e8cb86eab7104e4b144
S4mm4el <homer1996@gmail.com>
2022-10-20 17:05:25 +0300

    feat: add new fields of inputs to AddCardForm, css

--- src/components/AddCardForm/AddCardForm.tsx
+++ src/components/AddCardForm/AddCardForm.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './checkbox.css';
 import styles from './AddCardForm.module.css';
 
 function AddCardForm(): JSX.Element {
   return (
     <section className={styles.add__section}>
-      <div className={styles.add__header}>Create your card</div>
+      <h2 className={styles.add__header}>Create your card</h2>
       <form className={styles.add__table}>
         <label className={styles.add__text}>
           Card name:
@@ -13,32 +14,47 @@ function AddCardForm(): JSX.Element {
             Incorrect input!
           </span>
         </label>
-        <label className={styles.add__checkbox}>
+        <div className={styles.add__checkbox}>
           Card color:
-          <div className={styles.card__checkbox}>
-            <div style={{ backgroundColor: `blue` }} className={styles.card__color}></div>
-            <input type="checkbox" id="color-1" name="color" value="blue" />
-          </div>
-          <div className={styles.card__checkbox}>
-            <div style={{ backgroundColor: `red` }} className={styles.card__color}></div>
-            <input type="checkbox" id="color-2" name="color" value="red" />
-          </div>
-          <div className={styles.card__checkbox}>
-            <div style={{ backgroundColor: `green` }} className={styles.card__color}></div>
-            <input type="checkbox" id="color-3" name="color" value="green" />
-          </div>
-          <div className={styles.card__checkbox}>
-            <div style={{ backgroundColor: `black` }} className={styles.card__color}></div>
-            <input type="checkbox" id="color-4" name="color" value="black" />
-          </div>
-          <div className={styles.card__checkbox}>
-            <div style={{ backgroundColor: `white` }} className={styles.card__color}></div>
-            <input type="checkbox" id="color-5" name="color" value="white" />
+          <div className={styles.checkbox__wrapper}>
+            <div className={styles.card__checkbox}>
+              <label className={styles.color__label}>
+                <div style={{ backgroundColor: `blue` }} className={styles.card__color}></div>
+                <input type="checkbox" id="color-1" name="color" value="blue" />
+              </label>
+            </div>
+            <label className={styles.color__label}>
+              <div className={styles.card__checkbox}>
+                <div style={{ backgroundColor: `red` }} className={styles.card__color}></div>
+                <input type="checkbox" id="color-2" name="color" value="red" />
+              </div>
+            </label>
+
+            <label className={styles.color__label}>
+              <div className={styles.card__checkbox}>
+                <div style={{ backgroundColor: `green` }} className={styles.card__color}></div>
+                <input type="checkbox" id="color-3" name="color" value="green" />
+              </div>
+            </label>
+
+            <label className={styles.color__label}>
+              <div className={styles.card__checkbox}>
+                <div style={{ backgroundColor: `black` }} className={styles.card__color}></div>
+                <input type="checkbox" id="color-4" name="color" value="black" />
+              </div>
+            </label>
+
+            <label className={styles.color__label}>
+              <div className={styles.card__checkbox}>
+                <div style={{ backgroundColor: `white` }} className={styles.card__color}></div>
+                <input type="checkbox" id="color-5" name="color" value="white" />
+              </div>
+            </label>
           </div>
           <span className={styles.name} style={{ display: 'none' }}>
             Color set to none!
           </span>
-        </label>
+        </div>
         <label className={styles.add__text}>
           Card type:
           <select name="" id="card-type">
@@ -54,14 +70,32 @@ function AddCardForm(): JSX.Element {
         </label>
         <label className={styles.add__text}>
           Card adding date:
-          <input type="date" className={styles.textbox} />
+          <input
+            type="date"
+            defaultValue={new Date().toISOString().slice(0, 10)}
+            max="2025-12-31"
+            className={styles.textbox}
+          />
           <span className={styles.name} style={{ display: 'none' }}>
             Enter the date!
           </span>
         </label>
         <label className={styles.add__checkbox}>
           Availability:
-          <input type="checkbox" id="color-1" name="color" value="blue" />
+          {/* <!-- Toggle Button Style 8 --> */}
+          <label className="toggler-wrapper style-9">
+            <input type="checkbox" />
+            <div className="toggler-slider">
+              <div className="toggler-knob"></div>
+            </div>
+          </label>
+        </label>
+        <label className={styles.add__text}>
+          Url to card image:
+          <input type="text" className={styles.textbox} defaultValue={''} />
+          <span className={styles.name} style={{ display: 'none' }}>
+            Enter image url!
+          </span>
         </label>
       </form>
     </section>
