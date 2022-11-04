c417fe0c3cb5e077c33ad28d73397d2424072ff0
S4mm4el <homer1996@gmail.com>
2022-11-04 15:08:59 +0300

    feat: add card imageUrl check

--- src/components/SearchResultFetch/SearchResultFetch.tsx
+++ src/components/SearchResultFetch/SearchResultFetch.tsx
@@ -3,7 +3,7 @@ import styles from '../Card/Card.module.css';
 import { Card } from 'components/Card/Card';
 
 function SearchResultFetch({ url = 'https://api.magicthegathering.io/v1/cards' }) {
-  const [cardsList, setCardsList] = useState<{ cards: { name: string }[] }>();
+  const [cardsList, setCardsList] = useState<{ cards: { name: string; imageUrl: string }[] }>();
 
   useEffect(() => {
     fetch(url)
@@ -12,17 +12,16 @@ function SearchResultFetch({ url = 'https://api.magicthegathering.io/v1/cards' }
       })
       .then((data) => {
         setCardsList(data);
-        if (cardsList) {
-          console.log(cardsList.cards);
-        }
-      })
-      .then(() => {
-        console.log(cardsList?.cards[0]);
       });
   });
   return (
     <section className={styles.card__section}>
-      {cardsList && cardsList.cards.map((item, index) => <Card key={index} {...item} />)}
+      {cardsList &&
+        cardsList.cards.map((item, index) => {
+          if (item.imageUrl) {
+            return <Card key={index} {...item} />;
+          }
+        })}
     </section>
   );
 }
