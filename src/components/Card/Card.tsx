602cf5f8a07d02e7108361e496d51a16ad6aa1a9
S4mm4el <homer1996@gmail.com>
2022-11-17 15:17:44 +0300

    feat: modal window functionality done

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,88 +5,108 @@ import { ICard } from './ICard';
 
 export function Card({ id, name, types, colors, imageUrl, image, min }: ICard): JSX.Element {
   const [modalOpen, setModalOpen] = useState(false);
-  min = true;
+
   function openModal() {
     if (min) {
       setModalOpen(true);
     }
   }
 
-  function closeModal() {
-    modalOpen ? setModalOpen(false) : setModalOpen(true);
+  function closeModal(e: React.MouseEvent<HTMLDivElement>) {
+    const element = e.target as HTMLDivElement;
+    if (!element.classList.contains(styles.card))
+      modalOpen ? setModalOpen(false) : setModalOpen(true);
     console.log(modalOpen);
   }
 
   return (
-    <div id="card-wrapper">
-      {modalOpen && <div onClick={closeModal}>X</div>}
-      <div
-        id={`card-${id}`}
-        onClick={openModal}
-        style={{
-          background: `linear-gradient(180deg, #ffffff 0%, ${colors?.map((color: string) => {
-            switch (color) {
-              case 'W':
-                color = 'white';
-                break;
-              case 'B':
-                color = 'black';
-                break;
-              case 'G':
-                color = 'green';
-                break;
-              case 'R':
-                color = 'red';
-                break;
-              default:
-                color = 'blue';
-            }
-            return color;
-          })} 120%)`,
-        }}
-        className={`${styles.card} ${modalOpen ? styles.modal : ''}`}
-      >
-        <h5 className={styles.card__title}>{name}</h5>
-        <img
-          style={min ? { display: 'none' } : { display: 'block' }}
-          className={styles.card__img}
-          width={200}
-          src={`${image ? URL.createObjectURL(image) : imageUrl}`}
-          alt={name}
-        />
-        <span className={styles.card__header}>Colors:</span>
-        <div className={styles.color__wrapper}>
-          {colors?.map((color: string) => {
-            switch (color) {
-              case 'W':
-                color = 'white';
-                break;
-              case 'B':
-                color = 'black';
-                break;
-              case 'G':
-                color = 'green';
-                break;
-              case 'R':
-                color = 'red';
-                break;
-              default:
-                color = 'blue';
-            }
-            return (
-              <div
-                style={{ backgroundColor: `${color}` }}
-                key={color}
-                className={styles.card__color}
-              ></div>
-            );
-          })}
-        </div>
+    <div
+      onClick={(e) => {
+        closeModal(e);
+      }}
+      id="card-wrapper"
+      className={modalOpen ? styles.open_modal : ''}
+    >
+      <div className={styles.modal_wrapper}>
+        {modalOpen && (
+          <div
+            onClick={(e) => {
+              closeModal(e);
+            }}
+            className={styles.modal__cross}
+            id="modal-cross"
+          >
+            X
+          </div>
+        )}
+        <div
+          id={`card-${id}`}
+          onClick={openModal}
+          style={{
+            background: `linear-gradient(180deg, #ffffff 0%, ${colors?.map((color: string) => {
+              switch (color) {
+                case 'W':
+                  color = 'white';
+                  break;
+                case 'B':
+                  color = 'black';
+                  break;
+                case 'G':
+                  color = 'green';
+                  break;
+                case 'R':
+                  color = 'red';
+                  break;
+                default:
+                  color = 'blue';
+              }
+              return color;
+            })} 120%)`,
+          }}
+          className={`${styles.card} ${modalOpen ? styles.modal : ''}`}
+        >
+          <h5 className={styles.card__title}>{name}</h5>
+          <img
+            style={min && !modalOpen ? { display: 'none' } : { display: 'block' }}
+            className={styles.card__img}
+            width={200}
+            src={`${image ? URL.createObjectURL(image) : imageUrl}`}
+            alt={name}
+          />
+          <span className={styles.card__header}>Colors:</span>
+          <div className={styles.color__wrapper}>
+            {colors?.map((color: string) => {
+              switch (color) {
+                case 'W':
+                  color = 'white';
+                  break;
+                case 'B':
+                  color = 'black';
+                  break;
+                case 'G':
+                  color = 'green';
+                  break;
+                case 'R':
+                  color = 'red';
+                  break;
+                default:
+                  color = 'blue';
+              }
+              return (
+                <div
+                  style={{ backgroundColor: `${color}` }}
+                  key={color}
+                  className={styles.card__color}
+                ></div>
+              );
+            })}
+          </div>
 
-        <span className={styles.card__header}>Type:</span>
-        {types?.map((type: string) => (
-          <b key={type}>{type}</b>
-        ))}
+          <span className={styles.card__header}>Type:</span>
+          {types?.map((type: string) => (
+            <b key={type}>{type}</b>
+          ))}
+        </div>
       </div>
     </div>
   );
