7dfaa30a8128fa2d4d186599af25950b5eaea509
Semion Krapivin <homer1996@gmail.com>
2022-12-13 20:44:12 +0300

    fix: GlobalContext url getter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header/Header';
 import Main from './pages/Main/Main';
 import NotFound from './pages/NotFound/NotFound';
@@ -7,10 +7,15 @@ import stylesApp from './App.module.css';
 import styles from './components/Header/Header.module.css';
 import { Route, Routes, useLocation, Link } from 'react-router-dom';
 import AddCardForm from './pages/AddCardForm/AddCardForm';
+import { GlobalContext, useGlobalContext } from 'contexts/Context';
 
 function App() {
   const location = useLocation();
   const path: string = location.pathname;
+  const { url, setUrl } = useGlobalContext();
+  // const page = Math.floor(Math.random() * (100 - 1)) + 1;
+  // const pageSize = 10;
+  // const initialUrl = `https://api.magicthegathering.io/v1/cards?page=${page}&pageSize=${pageSize}`;
 
   function getCurrentTitle() {
     switch (path) {
@@ -25,24 +30,24 @@ function App() {
     }
   }
 
-  const saveCards = (url: string) => {};
-
   return (
-    <div className={stylesApp.App}>
-      <div className={styles.header}>
-        <Link id="main-page" to="/">
-          <img className={styles.logo} src="./logo.png" alt="logo" />
-        </Link>
-        <div className={styles.header__title}>{getCurrentTitle()}</div>
-        <Header />
+    <GlobalContext.Provider value={{ url, setUrl }}>
+      <div className={stylesApp.App}>
+        <div className={styles.header}>
+          <Link id="main-page" to="/">
+            <img className={styles.logo} src="./logo.png" alt="logo" />
+          </Link>
+          <div className={styles.header__title}>{getCurrentTitle()}</div>
+          <Header />
+        </div>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/addcard" element={<AddCardForm />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
       </div>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/addcard" element={<AddCardForm />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-    </div>
+    </GlobalContext.Provider>
   );
 }
 
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,20 +1,11 @@
-import { GlobalContext } from 'contexts/Context';
 import React, { useState } from 'react';
 import Search from '../../components/Search/Search';
 
 function Main(): JSX.Element {
-  const page = Math.floor(Math.random() * (100 - 1)) + 1;
-  const pageSize = 10;
-  const initialUrl = `https://api.magicthegathering.io/v1/cards?page=${page}&pageSize=${pageSize}`;
-
-  const [url, setUrl] = useState<string>(initialUrl);
-
   return (
-    <GlobalContext.Provider value={{ url, setUrl }}>
-      <main>
-        <Search />
-      </main>
-    </GlobalContext.Provider>
+    <main>
+      <Search />
+    </main>
   );
 }
 export default Main;
