c878a3dd3441e0f43668569ec8d15f004f359153
Semion Krapivin <homer1996@gmail.com>
2022-12-23 16:54:23 +0300

    feat: add loading images animation

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,7 +6,7 @@ import { GlobalContext } from 'contexts/Context';
 
 export function Card({ id, name, types, colors, imageUrl, image }: ICard): JSX.Element {
   const { state } = useContext(GlobalContext);
-  const [isLoaded, setIsLoaded] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(true);
   return (
     <div
       id={`card-${id}`}
@@ -34,14 +34,20 @@ export function Card({ id, name, types, colors, imageUrl, image }: ICard): JSX.E
       className={styles.card}
     >
       <h5 className={styles.card__title}>{name}</h5>
+      {isLoaded && (
+        <div className={styles.loader__container}>
+          <div className={styles.spinner}></div>
+        </div>
+      )}
       <img
         style={state.min ? { display: 'none' } : { display: 'block' }}
         className={`${styles.card__img}`}
         width={200}
         src={`${image ? URL.createObjectURL(image) : imageUrl}`}
         alt={name}
-        onLoad={() => console.log('loaded')}
+        onLoad={() => setIsLoaded(false)}
       />
+
       <span className={styles.card__header}>Colors:</span>
       <div className={styles.color__wrapper}>
         {colors?.map((color: string) => {
